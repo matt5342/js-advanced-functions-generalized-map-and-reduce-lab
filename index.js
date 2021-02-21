1b09dba67a806734e472d5c2540a53d4e1d8bf30
Matt <mattfsewell@gmail.com>
2021-02-20 20:00:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+// Your code here
+function map(array, func) {
+    let newarray = [];
+    for (var i = 0; i < array.length; i++) {
+        let a = array[i]
+
+        newarray.push(func(a));
+    }
+    return newarray;
+}
+function reduce(src, cb, starting){
+
+    let r = (starting) ? starting : src[0]
+    let i = (starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+}
+
+
